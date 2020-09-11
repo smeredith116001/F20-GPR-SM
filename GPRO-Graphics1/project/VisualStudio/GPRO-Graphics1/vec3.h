ff1d90cd2e192617f5d68162f35b0f720eec38ba
Khalil-Lyons <61299000+Khalil-Lyons@users.noreply.github.com>
2020-09-11 19:22:56 -0400

    Added operator overloads

--- GPRO-Graphics1/project/VisualStudio/GPRO-Graphics1/vec3.h
+++ GPRO-Graphics1/project/VisualStudio/GPRO-Graphics1/vec3.h
@@ -1,20 +1,24 @@
 #ifndef VEC3_H
 #define VEC3_H
-
+//includes
 #include <cmath>
 #include <iostream>
+//usings
 using namespace std;
 using std::sqrt;
 
 class vec3 {
 public:
+    //constructors
     vec3() : e{ 0,0,0 } {}
     vec3(double e0, double e1, double e2) : e{ e0, e1, e2 } {}
 
+    //return functions
     double x() const { return e[0]; }
     double y() const { return e[1]; }
     double z() const { return e[2]; }
 
+    //overload operators for vectors
     vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
     double operator[](int i) const { return e[i]; }
     double& operator[](int i) { return e[i]; }
@@ -37,6 +41,19 @@ public:
         return *this *= 1 / t;
     }
 
+    vec3& operator*(const double t) {
+       
+        return *this *= t;
+    }
+
+    vec3& operator/(const double t) {
+        e[0] /= t;
+        e[1] /= t;
+        e[2] /= t;
+        return *this;
+    }
+
+    //length functions
     double length() const {
         return sqrt(length_squared());
     }
